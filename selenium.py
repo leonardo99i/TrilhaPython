b4e50fa136fd227aa1f2c03b70af7e42c7e61076
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-11-04 00:15:31 -0300

    testes

---
+++ selenium.py
@@ -0,0 +1,8 @@
+import selenium
+from selenium import webdriver
+
+driver = webdriver.Chrome() #Isso vai iniciar o navegador Chrome
+driver.get('https://www.python.org/') #Isso ira acessar a URL especifica
+driver.find_element_by_css_selector('#id-search-field').send_keys("python") #Digita o texto "python" no input
+driver.find_element_by_css_selector('#submit').click() #Clica no botão submit
+driver.quit() #Isso ira encerrar o navegador
