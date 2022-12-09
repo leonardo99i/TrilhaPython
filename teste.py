f7e079195fc8548d7d5706928efa44704914b320
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-12-09 17:20:13 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,9 @@
+"//create a function to return the some of two integer number?"
+def solveMeFirst(a,b):
+
+   return a+b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+res = solveMeFirst(num1,num2)
+print(res)          
